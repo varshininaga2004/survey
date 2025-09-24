a0d34ecf88dda850c6af248ad607bfb8a6026f2b
varshini <varshininagapandi126@gmail.com>
2025-09-24 19:46:54 +0530

    update

--- surveyapp/views.py
+++ surveyapp/views.py
@@ -6,7 +6,6 @@ import json
 from django.http import JsonResponse
 from django.utils.safestring import mark_safe
 
-# Create your views here.
 
 def index(request):
     questions = Question.objects.all()
